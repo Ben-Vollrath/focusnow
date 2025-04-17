72b770bce36911eefb7276363c6e389eff379089
Ben Vollrath <ben.m.vollrath@gmail.com>
2025-04-17 19:15:56 +0200

    Add goal repository

---
+++ mobile/packages/goal_repository/lib/goal_repository.dart
@@ -0,0 +1,54 @@
+library goal_repository;
+
+import 'package:analytics_repository/analytics_repository.dart';
+import 'package:goal_repository/goal.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class GoalRepository {
+  final SupabaseClient supabaseClient;
+  final AnalyticsRepository _analyticsRepository;
+
+  GoalRepository({
+    SupabaseClient? supabaseClient,
+    AnalyticsRepository? analyticsRepository,
+  }) : supabaseClient = supabaseClient ?? Supabase.instance.client,
+       _analyticsRepository = analyticsRepository ?? AnalyticsRepository();
+
+  Future<Goal?> getGoal() async {
+    try {
+      final response =
+          await supabaseClient.from('goals').select('*').maybeSingle();
+
+      if (response == null) {
+        return null;
+      }
+
+      return Goal.fromJson(response);
+    } catch (e) {
+      throw Exception('Failed to fetch goal');
+    }
+  }
+
+  Future<void> deleteGoal(String goalId) async {
+    try {
+      await supabaseClient.from('goals').delete().eq('id', goalId);
+
+      _analyticsRepository.logEvent('goal_deleted');
+    } catch (e) {
+      throw Exception('Failed to delete goal: $e');
+    }
+  }
+
+  Future<void> createGoal(InputGoal inputGoal) async {
+    try {
+      final response = await supabaseClient.functions.invoke(
+        'create-goal',
+        body: inputGoal.toJson(),
+      );
+
+      _analyticsRepository.logEvent('goal_created');
+    } catch (e) {
+      throw Exception('Failed to create goal: $e');
+    }
+  }
+}
