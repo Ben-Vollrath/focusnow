d31f5aab1fe3e6da8e07e0ec1c3a84dd9bf13c14
Ben Vollrath <ben.m.vollrath@gmail.com>
2025-04-18 10:57:42 +0200

    Refactor tiles to use comon icon title

--- mobile/lib/ui/stats/study_chart.dart
+++ mobile/lib/ui/stats/study_chart.dart
@@ -2,6 +2,7 @@ import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:focusnow/ui/widgets/duration_text.dart';
 import 'package:focusnow/ui/widgets/flat_container.dart';
+import 'package:focusnow/ui/widgets/icon_title.dart';
 
 class StudyChart extends StatelessWidget {
   final List<Map<String, dynamic>> weeklyData;
@@ -54,7 +55,7 @@ class StudyChart extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text('Study Time'),
+          IconTitle(title: "Study Time", icon: Icons.hourglass_bottom_outlined),
           const SizedBox(height: 12),
           SizedBox(
             height: 160,
