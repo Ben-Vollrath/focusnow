35bb0e81356dee573a0a097fd1a9d758f81d7def
Ben Vollrath <ben.m.vollrath@gmail.com>
2025-04-21 11:04:01 +0200

    Update tests

--- supabase/functions/tests/handle-completed-study-session.test.ts
+++ supabase/functions/tests/handle-completed-study-session.test.ts
@@ -77,8 +77,8 @@ Deno.test("User XP is updated", async () => {
         assertEquals(fetchError, null);
         assertExists(userData?.[0]);
         assertEquals(userData?.[0].id, user.id);
-        assertEquals(userData?.[0].level, 2); //10 xp from session, 10 from daily sessions challenge, 10 from total sessions challenge
-        assertEquals(userData?.[0].xp, 5);
+        assertEquals(userData?.[0].level, 1); //10 xp from session, 10 from daily sessions challenge
+        assertEquals(userData?.[0].xp, 20);
     });
 });
 
@@ -168,8 +168,8 @@ Deno.test("Goal is updated but not completed", async () => {
         assertEquals(fetchErrorUsers, null);
         assertExists(userData?.[0]);
         assertEquals(userData?.[0].id, user.id);
-        assertEquals(userData?.[0].level, 2);
-        assertEquals(userData?.[0].xp, 5); //10 xp from session, 10 from daily sessions challenge, 10 from total sessions challenge
+        assertEquals(userData?.[0].level, 1);
+        assertEquals(userData?.[0].xp, 20); //10 xp from session, 10 from daily sessions challenge
     });
 });
 
@@ -207,7 +207,7 @@ Deno.test("Goal is completed and grants xp", async () => {
         assertEquals(goal?.[0].user_id, user.id);
         assertEquals(goal?.[0].current_minutes, 60);
         assertEquals(goal?.[0].completed, true);
-        assertEquals(goal?.[0].xp_reward, 3); // 1 xp for completing the goal
+        assertEquals(goal?.[0].xp_reward, 3); // 3 xp for completing the goal
 
         const { data: userData, error: fetchErrorUsers } = await client
             .from("users")
@@ -216,8 +216,8 @@ Deno.test("Goal is completed and grants xp", async () => {
         assertEquals(fetchErrorUsers, null);
         assertExists(userData?.[0]);
         assertEquals(userData?.[0].id, user.id);
-        assertEquals(userData?.[0].level, 4);
-        assertEquals(userData?.[0].xp, 3); //10 xp from session, 10 from daily sessions challenge, 10 from total sessions challenge, 10 from goal
+        assertEquals(userData?.[0].level, 3);
+        assertEquals(userData?.[0].xp, 28); //10 xp from session, 10 from daily sessions challenge, 3 from goal
     });
 });
 
@@ -241,8 +241,8 @@ Deno.test("Daily Sessions Challenge is completed and next one unlocked", async (
 
         assertEquals(fetchErrorUsers, null);
         assertExists(userData);
-        assertEquals(userData.level, 2);
-        assertEquals(userData.xp, 5); // 10 earned - 15 used to level up = 5 left
+        assertEquals(userData.level, 1);
+        assertEquals(userData.xp, 20); // 10 earned - +10 daily session challenge
 
         // Fetch challenge progress joined with challenge info
         const { data: challenges, error: fetchErrorChallenges } = await client
@@ -269,14 +269,35 @@ Deno.test("Daily Sessions Challenge is completed and next one unlocked", async (
 
 Deno.test("Total Sessions Challenge is completed and next one unlocked", async () => {
     await withAnonymousTestUser(async (client, user) => {
-        const { sessionData, data, error } = await createStudySession(
+        await createStudySession(
             client,
             edgeFunctionName,
             10,
         );
 
-        assertEquals(error, null);
-        assertEquals(data, "Sucess");
+        await createStudySession(
+            client,
+            edgeFunctionName,
+            10,
+        );
+
+        await createStudySession(
+            client,
+            edgeFunctionName,
+            10,
+        );
+
+        await createStudySession(
+            client,
+            edgeFunctionName,
+            10,
+        );
+
+        await createStudySession(
+            client,
+            edgeFunctionName,
+            10,
+        );
 
         // Validate user XP and level
         const { data: userData, error: fetchErrorUsers } = await client
@@ -287,8 +308,8 @@ Deno.test("Total Sessions Challenge is completed and next one unlocked", async (
 
         assertEquals(fetchErrorUsers, null);
         assertExists(userData);
-        assertEquals(userData.level, 2);
-        assertEquals(userData.xp, 5); // 10 earned - 15 used to level up = 5 left
+        assertEquals(userData.level, 4);
+        assertEquals(userData.xp, 20);
 
         // Fetch challenge progress joined with challenge info
         const { data: challenges, error: fetchErrorChallenges } = await client
@@ -299,17 +320,17 @@ Deno.test("Total Sessions Challenge is completed and next one unlocked", async (
         assertEquals(fetchErrorChallenges, null);
         assertExists(challenges);
 
-        // Filter daily_sessions only
-        const daily = challenges
+        // Filter total_sessions only
+        const total = challenges
             .filter((c) => c.challenge_id.category === "total_sessions")
             .sort((a, b) =>
                 a.challenge_id.difficulty - b.challenge_id.difficulty
             );
 
-        assertEquals(daily.length, 2); // one completed, one unlocked
-        assertEquals(daily[0].completed, true);
-        assertEquals(daily[1].completed, false);
-        assertEquals(daily[1].progress, 1);
+        assertEquals(total.length, 2); // one completed, one unlocked
+        assertEquals(total[0].completed, true);
+        assertEquals(total[1].completed, false);
+        assertEquals(total[1].progress, 5);
     });
 });
 
@@ -334,7 +355,7 @@ Deno.test("Total Hours Challenge is completed and next one unlocked", async () =
         assertEquals(fetchErrorUsers, null);
         assertExists(userData);
         assertEquals(userData.level, 4);
-        assertEquals(userData.xp, 10);
+        assertEquals(userData.xp, 0);
 
         // Fetch challenge progress joined with challenge info
         const { data: challenges, error: fetchErrorChallenges } = await client
@@ -377,7 +398,7 @@ Deno.test("Streak Days Challenge is completed and next one unlocked", async () =
                 study_date: yesterdayStr,
                 total_study_time: 25,
                 total_study_sessions: 1,
-                streak_day: 1,
+                streak_day: 2,
             });
 
         if (insertError.error) {
@@ -408,7 +429,7 @@ Deno.test("Streak Days Challenge is completed and next one unlocked", async () =
         assertEquals(userFetchError, null);
         assertExists(userData);
         assertEquals(userData.level, 2);
-        assertEquals(userData.xp, 15);
+        assertEquals(userData.xp, 10);
 
         // Fetch challenge progress joined with challenge info
         const { data: challenges, error: fetchErrorChallenges } = await client
@@ -429,6 +450,50 @@ Deno.test("Streak Days Challenge is completed and next one unlocked", async () =
         assertEquals(streak.length, 2); // one completed, one unlocked
         assertEquals(streak[0].completed, true);
         assertEquals(streak[1].completed, false);
-        assertEquals(streak[1].progress, 2); // streak_day is now 2
+        assertEquals(streak[1].progress, 3); // streak_day is now 2
+    });
+});
+
+Deno.test("Max Level is handled correctly", async () => {
+    await withAnonymousTestUser(async (client, user) => {
+        const serviceClient = getTestServiceClient();
+
+        const updateError = await serviceClient
+            .from("users")
+            .update({
+                level: 10,
+                xp: 60,
+            })
+            .eq("id", user.id);
+
+        if (updateError.error) {
+            console.error(
+                "Error update user level:",
+                updateError.error,
+            );
+            throw updateError.error;
+        }
+
+        // Create a new session for today
+        const { sessionData, data, error } = await createStudySession(
+            client,
+            edgeFunctionName,
+            10,
+        );
+
+        assertEquals(error, null);
+        assertEquals(data, "Sucess");
+
+        // Validate user state
+        const { data: userData, error: userFetchError } = await client
+            .from("users")
+            .select("*")
+            .eq("id", user.id)
+            .single();
+
+        assertEquals(userFetchError, null);
+        assertExists(userData);
+        assertEquals(userData.level, 10);
+        assertEquals(userData.xp, 80);
     });
 });
